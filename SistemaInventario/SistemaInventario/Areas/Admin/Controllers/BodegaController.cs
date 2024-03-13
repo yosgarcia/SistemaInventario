a02c100978d5c041a6a468f7f563490f7cca5218
Yosward Garcia <yosward.gar@gmail.com>
2024-03-13 11:51:43 -0600

    formulario de actualizar y crear bodega

---
+++ SistemaInventario/SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        // llama a la vista index
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Bodega bodega = new Bodega();
+            if(id == null)
+            {
+                // Crear una nueva bodega
+                bodega.Estado = true;
+                return View(bodega);
+            }
+            // Actualizamos bodega
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if(bodega == null)
+            {
+                return NotFound();
+            }
+            return View(bodega);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Bodega bodega)
+        {
+            if(ModelState.IsValid)
+            {
+                if(bodega.Id == 0)
+                {
+                    await _unidadTrabajo.Bodega.Agregar(bodega);
+                }
+                else
+                {
+                    _unidadTrabajo.Bodega.Actualizar(bodega);
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bodega);
+        }
+
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos}); // Aqui es con el nombre con el que se va a referenciar en el javascript
+        }
+
+        #endregion
+
+    }
+}
---
+++ SistemaInventario/SistemaInventario/wwwroot/js/bodega.js
@@ -0,0 +1,59 @@
+﻿let datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Bodega/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre", "width": "20%" },
+            { "data": "descripcion", "width": "40%" },
+            {
+                "data": "estado",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    }
+                    else {
+                        return "Inactivo";
+                    }
+                }, "width": "20%" 
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Bodega/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick=Delete("/Admin/Bodega/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                <i class="bi bi-trash3-fill"></i>
+                            </a>
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
+        ]
+
+    });
+}
